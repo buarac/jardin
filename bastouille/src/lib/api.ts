43d75a20859bc48048478f72e86b9fa84858448d
Sacha <sacha.stefanovic@me.com>
2025-07-28 18:46:05 +0200

    fix: page culture + ajout api.ts

---
+++ bastouille/src/lib/api.ts
@@ -0,0 +1,126 @@
+// src/lib/api.ts
+
+export async function fetchCultures() {
+  return [
+    {
+      id: "a091b9fa-4155-461c-aa82-efb38643f92b",
+      nom: "Abricot",
+      categorie: "fruit",
+      img: "abricot.png"
+    },
+    {
+      id: "2a75a7b2-92df-4027-b909-ee59af0fc2a6",
+      nom: "Ail",
+      categorie: "legume",
+      img: "ail.png"
+    },
+    {
+      id: "873c82de-f4a8-46be-8309-f253444a068c",
+      nom: "Aubergine",
+      categorie: "legume",
+      img: "aubergine.png"
+    },
+    {
+      id: "a77d83e5-7c41-4dde-8574-da38a54ed10b",
+      nom: "Basilic",
+      categorie: "aromatique",
+      img: "basilic.png"
+    },
+    {
+      id: "9b8c85d2-aff7-4ea2-b17e-77b7580367f9",
+      nom: "Betterave",
+      categorie: "legume",
+      img: "betterave.png"
+    },
+    {
+      id: "6c94cca7-5fcb-4942-a413-cac640f467b0",
+      nom: "Carotte",
+      categorie: "legume",
+      img: "carotte.png"
+    },
+    {
+      id: "fc47cc52-3850-4c74-8d36-7140b1f93c01",
+      nom: "Celeri feuille",
+      categorie: "legume",
+      img: "celeri_feuille.png"
+    },
+    {
+      id: "853cf710-77fd-4dcc-88da-8d82068faec2",
+      nom: "Celeri rave",
+      categorie: "legume",
+      img: "celeri_rave.png"
+    },
+    {
+      id: "b280fd54-948c-410f-9989-4b10da118df7",
+      nom: "Cerise",
+      categorie: "fruit",
+      img: "cerise.png"
+    },
+    {
+      id: "ca5a17ed-8dd8-415a-a764-1edb04036ee9",
+      nom: "Concombre",
+      categorie: "legume",
+      img: "concombre.png"
+    },
+    {
+      id: "231ea9fd-0e18-466b-a484-a8be07bc22b1",
+      nom: "Courgette",
+      categorie: "legume",
+      img: "courgette.png"
+    },
+    {
+      id: "d4a25a18-2bb1-4310-9669-c5ec902d3212",
+      nom: "Fraise",
+      categorie: "fruit",
+      img: "fraise.png"
+    },
+    {
+      id: "b7ad78bf-79aa-43cc-b3bb-d9d766a4d65a",
+      nom: "Framboise",
+      categorie: "fruit",
+      img: "framboise.png"
+    },
+    {
+      id: "1860accc-40c0-4431-8f21-aacc0f7b103e",
+      nom: "Haricot vert",
+      categorie: "legume",
+      img: "haricot_vert.png"
+    },
+    {
+      id: "0d34526b-d2ab-4650-9b70-ff17d533d844",
+      nom: "Kiwi",
+      categorie: "fruit",
+      img: "kiwi.png"
+    },
+    {
+      id: "d8c6ac46-d60e-473b-b282-a02dea13b304",
+      nom: "Melon",
+      categorie: "fruit",
+      img: "melon.png"
+    },
+    {
+      id: "03758f8a-d75d-4fac-974d-e93bfef28031",
+      nom: "Mure",
+      categorie: "fruit",
+      img: "mure.png"
+    },
+    {
+      id: "edf81acc-342b-4a86-b189-bd098967f18c",
+      nom: "Oignon",
+      categorie: "legume",
+      img: "oignon.png"
+    },
+    {
+      id: "fcc9e6da-eb97-40e4-8fe1-04a2a864c34d",
+      nom: "Pasteque",
+      categorie: "fruit",
+      img: "pasteque.png"
+    },
+    {
+      id: "22ef9519-c793-4313-af6a-f68e86a6dc71",
+      nom: "Patate douce",
+      categorie: "legume",
+      img: "patate_douce.png"
+    }
+  ];
+}
